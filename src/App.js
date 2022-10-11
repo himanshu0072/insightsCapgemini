afce8acd91e61a2d36a1a667de42f4a0ae8dbbe6
thisisaj1999 <atuljoshi94170@gmail.com>
2022-10-11 12:45:43 +0530

    initial setup done.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>H: Leading sustainability</p>
+      <p>A: Latest reports from the Capgemini Research Institute</p>
+      <p>H: Conversations for Tomorrow</p>
+      <p>A: Discover more</p>
+      <p>A: Stay informed</p>
+      {/* A will add button, H will add icons */}
     </div>
   );
 }
